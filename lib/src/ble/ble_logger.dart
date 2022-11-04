1c3ea121d4c74aecbd1835b4bb609319adbb8d4c
Jat Prakash Chandra (MS/EAC4-AE) <tjt1cob@bosch.com>
2022-11-04 10:09:28 +0530

    Initial commit

---
+++ lib/src/ble/ble_logger.dart
@@ -0,0 +1,13 @@
+class BleLogger {
+  final List<String> _logMessages = [];
+  final DateFormat formatter = DateFormat('HH:mm:ss.SSS');
+
+  List<String> get messages => _logMessages;
+
+  void addToLog(String message) {
+    final now = DateTime.now();
+    _logMessages.add('${formatter.format(now)} - $message');
+  }
+
+  void clearLogs() => _logMessages.clear();
+}
